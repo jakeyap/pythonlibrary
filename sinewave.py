493211d059e35b310ed0c85a4f5ea7410695e213
jake <jakeyap@hotmail.com>
2019-04-17 15:08:20 +0800

    modified sinewave
    converted sinewave file into functions
    cleaned up the code

--- sinewave.py
+++ sinewave.py
@@ -6,20 +6,51 @@ Created on Thu Dec 10 11:18:03 2015
 """
 
 import matplotlib.pyplot as plt
+import scipy
 
-counter = 0
-x = []
-y = []
-corrupt = []
-while counter < 1000:
-    x.append(counter/100)
-    counter = counter + 1
-    y.append(sin(counter/100*pi))
-    corrupt.append(sin(counter/100*pi) + 0.5*(rand()-0.5))
+#counter = 0
+#x = []
+#y = []
+#corrupt = []
+#while counter < 1000:
+#    x.append(counter/100)
+#    counter = counter + 1
+#    y.append(scipy.sin(counter/100*scipy.pi))
+#    corrupt.append(scipy.sin(counter/100*scipy.pi) + 0.5*(scipy.rand()-0.5))
+
+def coswave(wave_freq=2, samp_freq=100, amp=1, time=1, time_offset=0):
+    t = []
+    y = []
+        
+    counter = 0
+    end_index = time * samp_freq
     
-plt.subplot(1,2,1)
-plt.plot(x,y)
-plt.ylim([-2,2])
-plt.subplot(1,2,2)
-plt.plot(x,corrupt)
-plt.ylim([-2,2])
+    while (counter < end_index):
+        timestep = counter / samp_freq - time_offset
+        t.append(timestep)
+        y.append(amp * scipy.cos( scipy.pi * 2 * timestep * wave_freq))
+        counter = counter + 1
+        
+    return [t,y]
+
+def sinwave(wave_freq=2, samp_freq=100, amp=1, time=1, time_offset=0):
+    t = []
+    y = []
+        
+    counter = 0
+    end_index = time * samp_freq
+    
+    while (counter < end_index):
+        timestep = counter / samp_freq - time_offset
+        t.append(timestep)
+        y.append(amp * scipy.sin( scipy.pi * 2 * timestep * wave_freq))
+        counter = counter + 1
+        
+    return [t,y]
+
+
+[x,y] = coswave(3,150,1,1)
+plt.plot(x,y,'.-')
+
+[x,y] = sinwave(3,150,1,1)
+plt.plot(x,y,'x-')
